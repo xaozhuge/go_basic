d2d14b6ef6f625e6f495eecbfda7ebaf310446f7
zzz <xaozhuge@163.com>
2023-12-17 11:00:29 +0800

    type_struct.go

--- code/demo/type_struct.go
+++ code/demo/type_struct.go
@@ -8,49 +8,63 @@ type Person struct {
     Age  int
 }
 
-func createStruct() {
-    // 创建结构体实例
-    person1 := Person{"John", 25}
-    person2 := Person{Name: "Alice", Age: 30}
-
-    fmt.Println(person1) // 输出: {John 25}
-    fmt.Println(person2) // 输出: {Alice 30}
-    fmt.Println("-----------------")
+//接收器变量 p
+//接收器类型 Person
+func (p Person) changeName() {
+    p.Name = "李四"
+    fmt.Println(p.Name)
 }
 
-func queryStruct() {
+func createStruct() {
+    fmt.Println("createStruct()-----------------")
     person := Person{"John", 25}
+    fmt.Println(person) // 输出: {John 25}
+    fmt.Println()
+}
+
+func selectStructElement() {
+    fmt.Println("selectStructElement()-----------------")
+    person := Person{"John", 25}
+    fmt.Println(person)
+    fmt.Println("-------------")
     // 读取结构体字段
     fmt.Println("Name:", person.Name)
+    fmt.Println("-------------")
     fmt.Println("Age:", person.Age)
-    fmt.Println("-----------------")
+    fmt.Println()
 }
 
 
-func updateStruct() {
+func updateStructElement() {
+    fmt.Println("updateStructElement()-----------------")
     person := Person{"John", 25}
+    fmt.Println(person)
+    fmt.Println("-------------")
+
     // 修改结构体字段
     person.Name = "Alice"
     person.Age = 30
     fmt.Println(person) // 输出: {Alice 30}
-    fmt.Println("-----------------")
+    fmt.Println()
 }
 
 //结构体的字段是固定的，不能直接删除字段。
 //如果需要删除字段，可以通过创建一个新的结构体实例，只包含需要的字段，来模拟删除操作。
-func deleteStruct() {
+func deleteStructElement() {
+    fmt.Println("deleteStructElement()-----------------")
     person1 := Person{"John", 25}
+    fmt.Println(person1)
+    fmt.Println("-------------")
     // 模拟删除 Name 字段
     person2 := Person{Age: person1.Age}
-
     fmt.Println(person2)
 }
 
 func main() {
 	createStruct()
-	queryStruct()
-	updateStruct()
-	deleteStruct()
+	selectStructElement()
+	updateStructElement()
+	deleteStructElement()
 }
 
 /*
@@ -59,4 +73,15 @@ docker exec go_c go run demo/type_struct.go
 
 /*
 1. 结构体是一种用户自定义的数据类型，用于表示一组相关的字段。
+2. 方法的定义方式是 func (receiver Type) methodName(parameters) returnType。
+3. 在这里，(p Person) 是一个接收者（receiver），它指定了这个方法属于哪种类型。
+在这个特定的例子中，p 是一个接收者变量，它的类型是 Person
+4. func (p Person) changeName(){} 表示 changeName 是一个方法，属于 Person 类型的对象，
+其中 p 是一个变量名，表示在方法内部可以使用这个变量来引用调用该方法的 Person 实例。
+5. 如果写成 func changeName(p Person) {}，那就是一个函数而不是方法。
+在这种情况下，需要明确传递一个 Person 类型的实例作为参数，
+而在方法的写法中，接收者 p 就相当于函数的第一个参数。
+6. 方法的写法更符合面向对象的思想，因为它将操作数据和数据本身捆绑在一起。
+调用方法时，可以通过 . 运算符访问实例的字段，而不需要显式地传递实例作为参数。
+这种写法也使得代码更具可读性和一致性。
 */
