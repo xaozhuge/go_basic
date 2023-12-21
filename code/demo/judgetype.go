db57acf173317062d573f258fe5b70efc3330b2c
zzz <xaozhuge@163.com>
2023-12-21 09:17:13 +0800

    judgetype.go

--- code/demo/judgetype.go
+++ code/demo/judgetype.go
@@ -5,20 +5,45 @@ import (
 	"reflect"
 )
 
-func main() {
-	// 定义不同类型的变量
-	var integerVariable int = 42
-	var floatVariable float64 = 3.14
-	var stringVariable string = "Hello, World!"
-	var boolVariable bool = true
+func typeInt(){
+	fmt.Println("typeInt()------------")
+	temp_var := 42
+	fmt.Println("值为:", temp_var, "，类型为:", reflect.TypeOf(temp_var))
+	fmt.Println()
+}
 
-	// 使用 reflect.TypeOf() 判断变量类型
-	fmt.Println(reflect.TypeOf(integerVariable)) // int
-	fmt.Println(reflect.TypeOf(floatVariable))   // float64
-	fmt.Println(reflect.TypeOf(stringVariable))  // string
-	fmt.Println(reflect.TypeOf(boolVariable))    // bool
+func typeFloat64(){
+	fmt.Println("typeFloat64()------------")
+	temp_var := 3.14
+	fmt.Println("值为:", temp_var, "，类型为:", reflect.TypeOf(temp_var))
+	fmt.Println()
+}
+
+func typeString(){
+	fmt.Println("typeString()------------")
+	temp_var := "Hello, World!"
+	fmt.Println("值为:", temp_var, "，类型为:", reflect.TypeOf(temp_var))
+	fmt.Println()
+}
+
+func typeBool(){
+	fmt.Println("typeBool()------------")
+	temp_var := true
+	fmt.Println("值为:", temp_var, "，类型为:", reflect.TypeOf(temp_var))
+	fmt.Println()
+}
+
+func main() {
+	typeInt()
+	typeFloat64()
+	typeString()
+	typeBool()
 }
 
 /*
 docker exec go_c go run demo/judgetype.go
+*/
+
+/*
+1. 使用 reflect.TypeOf() 判断变量类型
 */
