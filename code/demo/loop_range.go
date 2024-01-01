e90e5ac3c839753099a9ea93ac982682c02de364
zzz <xaozhuge@163.com>
2024-01-01 18:42:09 +0800

    loop_range.go

--- code/demo/loop_range.go
+++ code/demo/loop_range.go
@@ -1,23 +1,57 @@
 package main
 
+import "code/P"
 import "fmt"
 
-func main() {
-    // 使用 range 迭代数组或切片
+// 使用 range 迭代数组或切片
+func rangeSlice(){
+    P.PE("rangeSlice")
     numbers := []int{1, 2, 3, 4, 5}
+    P.D(numbers)
+
     for index, value := range numbers {
         fmt.Printf("Index: %d, Value: %d\n", index, value)
     }
+}
 
-    fmt.Println("----------------")
-
+func rangeString(){
+    P.PE("rangeString")
     // 使用 range 迭代字符串
     greeting := "Hello, Go!"
+    P.D(greeting)
     for index, char := range greeting {
         fmt.Printf("Index: %d, Char: %c\n", index, char)
     }
 }
 
+func rangeMap(){
+    P.PE("rangeMap")
+
+    m2 := map[string]string{
+        "Sam":   "Male",
+        "Alice": "Female",
+    }
+    P.D(m2)
+
+    for key, value := range m2 {
+        P.D(key, value)
+    }
+}
+
+func main() {
+    rangeSlice()
+    rangeString()
+    rangeMap()
+
+}
+
 /*
 docker exec go_c go run demo/loop_range.go
 */
+
+/*
+1. %d 用于格式化输出整数
+2. %c 用于字符
+3. range 循环按照Unicode码点来迭代字符串，而不是按照字节。
+由于UTF-8的变长编码特性，一些字符占用的字节数可能不同。
+ */
