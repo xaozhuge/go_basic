da34d88a981cda5bbb7c72cf32bf3b69be699b47
zzz <xaozhuge@163.com>
2024-01-03 09:16:39 +0800

    实例转化为接口

--- code/demo/interface_use.go
+++ code/demo/interface_use.go
@@ -20,10 +20,13 @@ type Worker struct {
     gender string
 }
 
-
 func (w *Worker) getName() string {
     return w.name+"456"
 }
+
+// 实例转化为接口
+var _ Person = (*Student)(nil)
+var _ Person = (*Worker)(nil)
 
 func selectInterface(){
     P.PE("selectInterface")
@@ -38,6 +41,8 @@ func selectInterface(){
     P.D(m)
     P.D(m.getName()) // Tom
 }
+
+
 
 func main() {
     selectInterface()
@@ -54,6 +59,10 @@ docker exec go_c go run demo/interface_use.go
 1. 注意结构体的简写和不简写
 2. p作为接口无法直接取值，例如p.name，通过方法来实现
 3. p 通过哪个结构体实现，使用的是哪个结构体的方法
+4. 可以用以下去监测 某个类型是否实现了某个接口的所有方法
+var _ Person = (*Student)(nil)
+将空值 nil 转换为 *Student 类型，再转换为 Person 接口，
+如果转换失败，说明 Student 并没有实现 Person 接口的所有方法。
 
 
 */
