5a677d3b0aa6f369a64be653db381e747d802a60
zzz <xaozhuge@163.com>
2024-01-07 18:12:43 +0800

    calc.go

--- code/unit_test/calc.go
+++ code/unit_test/calc.go
@@ -1,15 +1,7 @@
 // calc.go
 package main
-import "fmt"
 
 func add(num1, num2 int) int {
 	return num1 + num2
 }
 
-func main() {
-	fmt.Println(add(2,3))
-}
-
-/*
-docker exec go_c go run demo/calc.go
-*/
